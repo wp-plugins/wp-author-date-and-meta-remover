424b71f9bb174d458d65835ef3064cd2438d6672
Johnnie2u <Johnnie2u@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-04-08 16:09:29 +0000

    Update of plugin and introduction of WP ARMR Pro+
    git-svn-id: https://plugins.svn.wordpress.org/wp-author-date-and-meta-remover/trunk@1130540 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-author-date-and-meta-remover.php
+++ wp-author-date-and-meta-remover.php
@@ -1,14 +1,14 @@
 <?php
 /**
  * Plugin Name: WP Author, Date and Meta Remover
- * Plugin URI: http://johnniejodelljr.com/wp-author-and-date-remover/
- * Description: Remove the "Posted on Month, Day Year by Author" on the home page and post. Just plug and play; it's that easy!
- * Version: 1.0.2
+ * Plugin URI: http://wpadmrproplus.com
+ * Description: Remove post meta data. Just plug and play; it's that easy! <a href="http://wpadmrproplus.com">Get WP ADMR Pro+ for Intelligent Meta Data Control.</a>
+ * Version: 1.0.3
  * Author: Johnnie J. O'Dell Jr.
- * Author URI: http://johnniejodelljr.com/
+ * Author URI: http://wpadmrproplus.com/contact-us/
  * License: GPL2
  */
- /*  Copyright 2014  Johnnie J. O'Dell Jr.  (email : johnnie@johnniejodelljr.com)
+ /*  Copyright 2014  Johnnie J. O'Dell Jr.  (email : johnniejodelljr@gmail.com)
 
     This program is free software; you can redistribute it and/or modify
     it under the terms of the GNU General Public License, version 2, as 
